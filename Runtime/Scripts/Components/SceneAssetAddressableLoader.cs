f490cd9e4414912d5bd8a30c0e7dcb175bf2a449
LCH <ckdgus4002@gmail.com>
2023-11-20 19:41:07 +0900

    #if UNITY_EDITOR 대응.

--- Runtime/Scripts/Components/SceneAssetAddressableLoader.cs
+++ Runtime/Scripts/Components/SceneAssetAddressableLoader.cs
@@ -3,18 +3,24 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 #if UNITY_EDITOR
 using UnityEditor;
+#else
+using UnityEngine;
 #endif
 
 namespace LCHFramework.Addressable.Components
 {
+#if UNITY_EDITOR
     public class SceneAssetAddressableLoader : AssetAddressableLoader<SceneAsset, SceneInstance>
+#else
+    public class SceneAssetAddressableLoader : AssetAddressableLoader<Object, SceneInstance>
+#endif
     {
         // UnityEvent event.
         public void OnClick() => LoadAsync();
         
         
         
-        protected override AsyncOperationHandle<SceneInstance> _LoadAsync() => Addressables.LoadSceneAsync(AssetAddress);
+        protected override AsyncOperationHandle<SceneInstance> GetLoadAsyncOperationHandle() => Addressables.LoadSceneAsync(AssetAddress);
 
         protected override void _Release() => Addressables.UnloadSceneAsync(AsyncOperationHandle);
     }
